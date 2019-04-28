28f3f9030516332a78ae92dafed822755e17ba74
David Chen <davidchen@d-techpro.net>
2019-04-28 09:57:04 +0800

    updated

--- docker-release.sh
+++ docker-release.sh
@@ -18,14 +18,14 @@ echo "version: $version"
 ./docker-build.sh
 
 # tag it
-git add -A
-git commit -m "version $version"
-git tag -a "$version" -m "version $version"
-git push
-git push --tags
+#git add -A
+#git commit -m "version $version"
+#git tag -a "$version" -m "version $version"
+#git push
+#git push --tags
 
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 
 # push it
-docker push $USERNAME/$IMAGE:latest
-docker push $USERNAME/$IMAGE:$version
+#docker push $USERNAME/$IMAGE:latest
+#docker push $USERNAME/$IMAGE:$version
